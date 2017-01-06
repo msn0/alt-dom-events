c51fa2fd6c8c4edffad5f28af88f26d278897be0
Michał Jezierski <ambinanitelo@gmail.com>
2017-01-07 00:03:28 +0100

    avajs & jsdom + new deps and get rid of karma

---
+++ test.js
@@ -0,0 +1,38 @@
+import {bindDOMEvents} from './dist/index';
+import test from 'ava';
+import jsdom from 'jsdom';
+
+test.beforeEach(t => {
+  global.document = jsdom.jsdom('<body></body>');
+  global.window = document.defaultView;
+});
+
+test.cb('should bind custom event', t => {
+  const target = {
+    callback: (data) => {
+      t.deepEqual(data, {foo: 'bar'});
+      t.end();
+    }
+  };
+
+  bindDOMEvents({'foo-event': 'callback'}, target);
+
+  document.dispatchEvent(
+    new window.CustomEvent('foo-event', {detail: {foo: 'bar'}})
+  );
+});
+
+test.cb('should support currying', t => {
+  const target = {
+    callback: (data) => {
+      t.deepEqual(data, {foo: 'bar'});
+      t.end();
+    }
+  };
+
+  bindDOMEvents({'foo-event': 'callback'})(target);
+
+  document.dispatchEvent(
+    new window.CustomEvent('foo-event', {detail: {foo: 'bar'}})
+  );
+});
